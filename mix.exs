5be26812d8c5a12aad7720ef4adc3f385239e893
Michael Wu <me@miXwui.com>
2022-12-02 11:04:12 -0600

    Use own erlport fork: deprecated getargspec -> getfullargspec

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Export.Mixfile do
 
   defp deps() do
     [
-      {:erlport, "~> 0.10"},
+      {:erlport, git: "https://github.com/miXwui/erlport.git"},
       {:ex_doc, ">= 0.0.0", only: :dev},
     ]
   end
